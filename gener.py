b98b5eee816f53ed084120a2e934e249484b6c60
AlexGrom1989 <94692477+AlexGrom1989@users.noreply.github.com>
2023-04-05 22:21:01 +0300

    Add files via upload

--- gener.py
+++ gener.py
@@ -1,5 +1,8 @@
-print('stop = выход.\nскип = пропустить.\nдальше изи = выбрать другой раздел и завершить текущий.')
+﻿print('stop = выход.\nскип = пропустить.\nдальше изи = выбрать другой раздел и завершить текущий.')
 from random import choice
+def ret(lkm, tmp):
+    if name[tmp] == '#Наречия': return lkm
+    return lkm.split()
 with open('words.txt') as text:
     data, name = [], []
     for t in text:
@@ -11,6 +14,7 @@ with open('words.txt') as text:
     data = {x[0]:x[1:] for x in data}
     com = ''
     while 'stop' not in com:
+        negative = set()
         print('Выбери: ')
         for i in range(len(name)): print(i + 1, '->', name[i][1:])
         tmp = int(input()) - 1
@@ -18,22 +22,23 @@ with open('words.txt') as text:
         if name[tmp] != '#Ударения': kop = [i.lower() for i in kop]
         while kop:
             lkm = choice(kop)
-            if name[tmp] == '#Ударения': print(lkm.lower(), ' '*11, len(kop))
+            if name[tmp] == '#Наречия': print(lkm.replace(' ', '').lower(), ' '*11, len(kop))
+            elif name[tmp] == '#Ударения': print(lkm.lower(), ' '*11, len(kop))
             elif name[tmp] == '#ННН': print(lkm.replace('нн', '_').replace('н', '_'), ' '*11, len(kop))
             else: print(*[[i, '_'][i in 'уеоаияюэы'] for i in lkm], ' '*13, len(kop), sep='')
             com = input().strip()
             if 'скип' in com:
                 kop.remove(lkm)
                 continue
-            if com in lkm.split(): kop.remove(lkm)
-            elif 'stop' in com or 'дальше изи' in com: break
-            else:
-                while com not in lkm.split() and 'stop' not in com and \
-                  'скип' not in com and 'дальше изи' not in com:
-                    print(lkm)
-                    com = input().strip()
+            if com in ret(lkm, tmp): kop.remove(lkm)
+            while com not in ret(lkm, tmp) and 'stop' not in com and \
+                'скип' not in com and 'дальше изи' not in com:
+                negative.add(lkm)
+                print(lkm)
+                com = input().strip()
             if 'stop' in com or 'дальше изи' in com: break
             if 'скип' in com:
                 kop.remove(lkm)
                 continue
+        print(negative)
 
